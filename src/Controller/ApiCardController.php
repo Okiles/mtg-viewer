e2cb241e07bd4221034e70cbf767b1445b1ed922
Okiles <colsonlenny5@gmail.com>
2025-03-17 18:09:29 +0100

    tkt

--- src/Controller/ApiCardController.php
+++ src/Controller/ApiCardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Artist;
 use App\Entity\Card;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -20,26 +19,48 @@ class ApiCardController extends AbstractController
         private readonly LoggerInterface $logger
     ) {
     }
-    #[Route('/all', name: 'List all cards', methods: ['GET'])]
-    #[OA\Put(description: 'Return all cards in the database')]
+
+    #[Route('/all', name: 'list_all_cards', methods: ['GET'])]
+    #[OA\Get(description: 'Return all cards in the database')]
     #[OA\Response(response: 200, description: 'List all cards')]
     public function cardAll(): Response
     {
+        $this->logger->info('Request received: Fetching all cards');
+
         $cards = $this->entityManager->getRepository(Card::class)->findAll();
+
+        if (!$cards) {
+            $this->logger->warning('No cards found in the database');
+            return $this->json(['message' => 'No cards found'], 404);
+        }
+
+        $this->logger->info('Successfully retrieved ' . count($cards) . ' cards');
         return $this->json($cards);
     }
 
-    #[Route('/{uuid}', name: 'Show card', methods: ['GET'])]
-    #[OA\Parameter(name: 'uuid', description: 'UUID of the card', in: 'path', required: true, schema: new OA\Schema(type: 'integer'))]
-    #[OA\Put(description: 'Get a card by UUID')]
+    #[Route('/{uuid}', name: 'show_card', methods: ['GET'])]
+    #[OA\Parameter(
+        name: 'uuid', 
+        description: 'UUID of the card', 
+        in: 'path', 
+        required: true, 
+        schema: new OA\Schema(type: 'string')
+    )]
+    #[OA\Get(description: 'Get a card by UUID')]
     #[OA\Response(response: 200, description: 'Show card')]
     #[OA\Response(response: 404, description: 'Card not found')]
     public function cardShow(string $uuid): Response
     {
+        $this->logger->info("Request received: Fetching card with UUID: {$uuid}");
+
         $card = $this->entityManager->getRepository(Card::class)->findOneBy(['uuid' => $uuid]);
+
         if (!$card) {
+            $this->logger->error("Card not found with UUID: {$uuid}");
             return $this->json(['error' => 'Card not found'], 404);
         }
+
+        $this->logger->info("Successfully retrieved card with UUID: {$uuid}");
         return $this->json($card);
     }
 }
